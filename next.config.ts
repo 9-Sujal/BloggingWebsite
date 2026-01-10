75762eee28d7607cb7d59ef9925e18081ad00c74
9-Sujal <sujalghorse9@outlook.com>
2026-01-11 01:00:57 +0530

    fix: upgrade Next.js to latest patched version

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  eslint:{
-    ignoreDuringBuilds:true,
-  },
   env: {
     API:
       process.env.NODE_ENV === "production"
