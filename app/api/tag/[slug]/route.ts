47a2cae588e9879bf4a2cf4c610bcca3832531b9
9-Sujal <sujalghorse9@outlook.com>
2026-01-10 19:17:46 +0530

    updated error type

---
+++ app/api/tag/[slug]/route.ts
@@ -0,0 +1,42 @@
+import dbConnect from "@/lib/dbConnect";
+import Blog from "@/models/blog";
+import Tag from "@/models/tag";
+import { NextResponse } from "next/server";
+
+
+
+
+export async function GET(req: Request, context: { params: { slug: string } }) {
+    await dbConnect();
+
+    const slug = context.params.slug;
+    const url = new URL(req.url);
+    const page = parseInt(url.searchParams.get("page") || "1", 10);
+    const pageSize = 6;
+
+  try{
+    const tag = await Tag.findOne({slug});
+    const skip = (page - 1) * pageSize;
+    const totalBlogs=await Blog.countDocuments({tags:tag?._id});
+
+    const blogs = await Blog.find({tags:tag?._id})
+    .select("-content")
+    .select("title excerpt slug featuredImage postedBy tags createdAt")
+    .populate("postedBy", "name")
+    .populate("tags", "name slug ")
+    .sort({createdAt:-1})
+    .skip(skip)
+    .limit(pageSize);
+
+    return NextResponse.json({
+        blogs,
+        totalPages: Math.ceil(totalBlogs/pageSize),
+        tag, 
+        page: Number(page),
+    }, {status:200});
+  }
+    catch(err:unknown){
+        const errorMessage = err instanceof Error ? err.message : "An unknown error occurred";
+        return NextResponse.json({err:errorMessage}, {status:500});
+    }
+}
